cd347c4b2b754b71f373b30e56e7d8d7fa235e1e
児玉 悠太朗 <kodama@kodama-yutaroMacBook-Air.local>
2016-06-07 22:36:16 +0900

    Revert "add_devise_view"
    This reverts commit 0b14aa5c123342b702cc2232843761a9c1e39c57.

--- db/migrate/20160607125620_add_member_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddMemberToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :member, :string
-  end
-end
--- db/migrate/20160607130204_remove_title_from_users.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTitleFromUsers < ActiveRecord::Migration
-  def change
-    remove_column :users, :title, :string
-  end
-end
