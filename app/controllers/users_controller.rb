1152c678291313ea23a2263dfd2c6c50872e6ae1
児玉 悠太朗 <kodama@kodama-yutaroMacBook-Air.local>
2016-06-09 00:11:17 +0900

    add_edit_function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,25 @@ class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
 	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		result = if current_user.id == @user.id
+			@user.update(update_params)
+		end
+
+		if result
+			sign_in(@user, bypass: true) if current_user.id == @user.id
+		end
+		redirect_to user_path
+	end
+
+	private
+	def update_params
+		params.require(:user).permit(:nickname, :avatar,  :password, :member, :profile, :work)
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'products#index'
   resources :products , only: [:index, :new, :show]
-  resources :users , only: :show
+  resources :users , only: [:show, :edit, :update]
 end
