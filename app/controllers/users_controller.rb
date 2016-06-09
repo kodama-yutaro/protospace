ef6626948785aeba3f98934c42a19211c6bfd59e
児玉 悠太朗 <kodama@kodama-yutaroMacBook-Air.local>
2016-06-09 19:54:26 +0900

    hash_space & make_method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,26 @@
 class UsersController < ApplicationController
+
+	before_action :set_user, only: [:show, :edit, :update]
+
 	def show
-		@user = User.find(params[:id])
 	end
 
 	def edit
-		@user = User.find(params[:id])
 	end
 
 	def update
-		@user = User.find(params[:id])
-		result = if current_user.id == @user.id
-			@user.update(update_params)
-		end
-
-		if result
-			sign_in(@user, bypass: true) if current_user.id == @user.id
-		end
+		current_user.id == @user.id
+		@user.update(update_params)
+		sign_in(@user, bypass: true)
 		redirect_to user_path
 	end
 
 	private
 	def update_params
-		params.require(:user).permit(:nickname, :avatar,  :password, :member, :profile, :work)
+		params.require(:user).permit(:nickname, :avatar, :member, :profile, :work)
+	end
+
+	def set_user
+		@user = User.find(params[:id])
 	end
 end
