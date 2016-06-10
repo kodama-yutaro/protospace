55178de3912b5ac295d8309df4e30156621fca32
児玉 悠太朗 <kodama@kodama-yutaroMacBook-Air.local>
2016-06-10 09:50:58 +0900

    change_indent

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,27 @@
 class UsersController < ApplicationController
 
-	before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update]
 
-	def show
-	end
+  def show
+  	hoeghgoehgoe
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def update
-		current_user.id == @user.id
-		@user.update(update_params)
-		sign_in(@user, bypass: true)
-		redirect_to user_path
-	end
+  def update
+	current_user.id == @user.id
+	@user.update(update_params)
+	sign_in(@user, bypass: true)
+	redirect_to user_path
+  end
 
-	private
-	def update_params
-		params.require(:user).permit(:nickname, :avatar, :member, :profile, :work)
-	end
+  private
+  def update_params
+	params.require(:user).permit(:nickname, :avatar, :member, :profile, :work)
+  end
 
-	def set_user
-		@user = User.find(params[:id])
-	end
+  def set_user
+	@user = User.find(params[:id])
+  end
 end
