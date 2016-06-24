b79314b96700294c883d6f0c2cf2caff927be60b
児玉 悠太朗 <kodama@kodama-yutaroMacBook-Air.local>
2016-06-24 13:01:49 +0900

    reload_product

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,21 @@
 class LikesController < ApplicationController
 
+  before_action :set_product
+
   def create
     @like = Like.create(user_id: current_user.id, product_id: params[:product_id])
-    @product = Product.find(params[:product_id])
+    @product.reload
   end
 
   def destroy
     @like = current_user.likes.find_by(product_id: params[:product_id])
     @like.destroy
+    @product.reload
+  end
+
+  private
+  def set_product
     @product = Product.find(params[:product_id])
   end
+
 end
