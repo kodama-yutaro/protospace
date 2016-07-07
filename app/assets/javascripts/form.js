34e2639b48a04384153fca1a0420b7f8689c30d4
児玉 悠太朗 <kodama@kodama-yutaroMacBook-Air.local>
2016-07-07 20:43:24 +0900

    refactoring js file

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,45 +1,15 @@
 $(window).on('page:change', function () {
-  $('#js-preview-images0').change(function(event){
+    var number = ["0","1","2","3"];
+ $.each(number, function(i){
+  $("#js-preview-images"+i).change(function(event){
     var file = event.target.files[0]
     var reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = function(e){
         var img = $("<img>");
         img.attr('src', e.target.result);
-        $("#js-preview-images0").empty().append(img);
-    };
+        $("#js-preview-images"+i).empty().append(img);
+   };
   });
-
-  $('#js-preview-images1').change(function(event){
-    var file = event.target.files[0]
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function(e){
-        var img = $("<img>");
-        img.attr('src', e.target.result);
-        $("#js-preview-images1").empty().append(img);
-    };
-  });
-
-  $('#js-preview-images2').change(function(event){
-    var file = event.target.files[0]
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function(e){
-        var img = $("<img>");
-        img.attr('src', e.target.result);
-        $("#js-preview-images2").empty().append(img);
-    };
-  });
-
-  $('#js-preview-images3').change(function(event){
-    var file = event.target.files[0]
-    var reader = new FileReader();
-    reader.readAsDataURL(file);
-    reader.onload = function(e){
-        var img = $("<img>");
-        img.attr('src', e.target.result);
-        $("#js-preview-images3").empty().append(img);
-    };
-  });
+ });
 });
